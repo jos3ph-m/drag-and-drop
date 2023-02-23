22af820ce3f8501dd9d95ab11100fd3f7a17af5b
Joseph M <jmedina1985@hotmail.com>
2023-02-22 19:29:43 -0800

    getting started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import {} from '@dnd-kit/core';
 
 function App() {
   return <h1>Hi</h1>;
