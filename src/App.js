eb0b8c5d92281c3a38f2f2e87b573dcdd2d8337d
Joseph M <jmedina1985@hotmail.com>
2023-02-24 21:41:14 -0800

    dnd set up

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ function App() {
     </DndContext>
   );
 
-  function handleDragEnd(event) {}
+  function handleDragEnd(event) {
+    console.log('Drag end called');
+  }
 }
 
 export default App;
