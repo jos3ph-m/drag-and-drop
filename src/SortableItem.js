ffac136789183e0da24758f30bd11aaefd9828d0
Joseph M <jmedina1985@hotmail.com>
2023-02-25 21:13:44 -0800

    create SortableItem

--- src/SortableItem.js
+++ src/SortableItem.js
@@ -2,4 +2,9 @@ import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import Card from 'react-bootstrap';
 
-export function SortableItem(props) {}
+export function SortableItem(props) {
+  // props.id
+  // JavaScript
+
+  useSortable();
+}
